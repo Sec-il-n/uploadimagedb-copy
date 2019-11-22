7cc9a03e5517c6fdf0da414ff0e8a129dd78c4fe
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-23 04:33:42 +0900

    Update ListKeysVersioningEnabledBucket.java

--- src/test/java/model4test/ListKeysVersioningEnabledBucket.java
+++ src/test/java/model4test/ListKeysVersioningEnabledBucket.java
@@ -19,50 +19,47 @@ import com.amazonaws.services.s3.model.VersionListing;
 public class ListKeysVersioningEnabledBucket {
 	private static Regions clientRegion = Regions.AP_NORTHEAST_1;
 	private static String bucketName="cec-il-n-aws";
-//	private static String fileObjKeyName;//fileObjKeyName
-//	private static AmazonS3 s3Client;
-//get ful list of versions
-	@BeforeAll//&@AfterAll
+
+	//get ful list of versions
+	@BeforeAll
 	public Map<String,String> getListvarsionig() {
 		Map<String,String> s3Keys=new HashMap<>();
 
 		try {
 			AmazonS3 s3client= AmazonS3ClientBuilder.standard()
-					.withCredentials(new ProfileCredentialsProvider())
-					.withRegion(clientRegion)
+				.withCredentials(new ProfileCredentialsProvider())
+				.withRegion(clientRegion)
 			        .build();
+			
 			ListVersionsRequest req= new ListVersionsRequest()
-					.withBucketName(bucketName)
-					.withMaxResults(2);
+				.withBucketName(bucketName)
+				.withMaxResults(2);
+			
 			VersionListing listing=s3client.listVersions(req);
-//			int numVersions=0, numPages=0;
+			
 			while(true) {
-//				numPages++;
 				for(S3VersionSummary summary :
 						listing.getVersionSummaries()) {
 					s3Keys.put(summary.getKey(), summary.getVersionId());
-//					numVersions++;
 				}
-				if(listing.isTruncated()) {//continous l47 'versionListing'
+				
+				if(listing.isTruncated()) {
 					listing=s3client.listNextBatchOfVersions(listing);
 
 				}else {
 					break;
 				}
 			}
+		
 		} catch (AmazonServiceException e) {
 			e.printStackTrace();
 		} catch (SdkClientException e) {
 			e.printStackTrace();
 		}
+		
 		return s3Keys;
 	}
 
 
-
-	@Test
-	void test() {
-//		fail("まだ実装されていません");
-	}
 
 }
