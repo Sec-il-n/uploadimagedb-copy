be5c3d7e3face7fb3c0707a5042823fd01d46f59
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-23 04:18:23 +0900

    indent modified 1123

--- src/test/java/model4test/DeleteObjectsforTest.java
+++ src/test/java/model4test/DeleteObjectsforTest.java
@@ -28,7 +28,6 @@ public class DeleteObjectsforTest {
 	static int successfulDeletes;
 
 	@BeforeAll
-//	public static void deleteObject(Map<String,String> toDelete) {
 	public static void deleteObject(List<String> toDelete) {
 		try {
 			s3=AmazonS3ClientBuilder.standard()
@@ -36,17 +35,19 @@ public class DeleteObjectsforTest {
 			.withRegion(clientRegion)
 			.build();
 			 String bucketVersionStatus = s3.getBucketVersioningConfiguration(bucketName).getStatus();
-			 if (!bucketVersionStatus.equals(BucketVersioningConfiguration.ENABLED)) {
+			 
+			if (!bucketVersionStatus.equals(BucketVersioningConfiguration.ENABLED)) {
 			     JOptionPane.showMessageDialog(new JDialog(), bucketName + " is not versioning-enabled !!");
 			 } else {
 				 deleteRemoveDeleteMarkers(objectsWithoutVersions(toDelete));
-//				 objectsWithVersions(toDelete);
 			 }
+		
 		} catch (SdkClientException | HeadlessException e) {
 			 JOptionPane.showMessageDialog(new JDialog(), " faild to delete object !! ");
 			e.printStackTrace();
 		}
 	}
+	
 	//with versions
 	public static void objectsWithVersions(Map<String,String> toDelete) {
 
@@ -54,66 +55,54 @@ public class DeleteObjectsforTest {
 		for(Map.Entry<String,String> d : toDelete.entrySet()) {
 			keys.add(new KeyVersion(d.getKey(),d.getValue()));
 		}
-		 DeleteObjectsRequest multiObjectDeleteRequest = new DeleteObjectsRequest(bucketName)
+		DeleteObjectsRequest multiObjectDeleteRequest = new DeleteObjectsRequest(bucketName)
 	                .withKeys(keys)
 	                .withQuiet(false);
-		 DeleteObjectsResult delObjRes = s3.deleteObjects(multiObjectDeleteRequest);
-	     successfulDeletes = delObjRes.getDeletedObjects().size();
-//	     assertEquals(successfulDeletes,8);//switch
-	     if(successfulDeletes==8) {
-	 			JOptionPane.showMessageDialog(new JDialog(), "roll back success!!");
-	 	 }else {
-	 			JOptionPane.showMessageDialog(new JDialog(), "faild!!");
-	     }
+		DeleteObjectsResult delObjRes = s3.deleteObjects(multiObjectDeleteRequest);
+	     	successfulDeletes = delObjRes.getDeletedObjects().size();
+	     	
+		if(successfulDeletes==8) {
+	 		JOptionPane.showMessageDialog(new JDialog(), "roll back success!!");
+	 	}else {
+	 		JOptionPane.showMessageDialog(new JDialog(), "faild!!");
+	     	}
 	}
-	//without versions
-
-
-//	public static String[] objectsWithoutVersions(Map<String,String> toDelete) {
-//		List<String> keysList=new ArrayList<>(toDelete.keySet());
-//		String[] keys= keysList.toArray(new String[keysList.size()]);
-////		ArrayList<KeyVersion> keys = new ArrayList<KeyVersion>();//use⤴instead
-//		return keys;
-//	}
+	
 	/**
 	 * delete
 	 */
 	public static DeleteObjectsResult objectsWithoutVersions(List<String> keysList) {
 		String[] keys= keysList.toArray(new String[keysList.size()]);
 		DeleteObjectsRequest multiObjectDeleteRequest =
-        		new DeleteObjectsRequest(bucketName).withKeys(keys).withQuiet(false);//When true, only errors will be returned in the service response.
-        DeleteObjectsResult delObjRes=s3.deleteObjects(multiObjectDeleteRequest);
-        successfulDeletes = delObjRes.getDeletedObjects().size();
-//        if(successfulDeletes==8) {
-        if(successfulDeletes==2) {
- 			JOptionPane.showMessageDialog(new JDialog(), "successfulDeletes!!");
-        }else {
- 			JOptionPane.showMessageDialog(new JDialog(), "faildDelete!!");
-        }
-//        assertEquals(successfulDeletes,8);//change if changed number of tests
-        return delObjRes;
+        	new DeleteObjectsRequest(bucketName).withKeys(keys).withQuiet(false);//When it's true, only errors will be returned in the service response.
+        	DeleteObjectsResult delObjRes=s3.deleteObjects(multiObjectDeleteRequest);
+        	successfulDeletes = delObjRes.getDeletedObjects().size();
+
+		if(successfulDeletes==2) {
+			JOptionPane.showMessageDialog(new JDialog(), "successfulDeletes!!");
+		}else {
+			JOptionPane.showMessageDialog(new JDialog(), "faildDelete!!");
+		}
+		return delObjRes;
 	}
 	/**
 	 * delete key mark
 	 */
 	public static void deleteRemoveDeleteMarkers(DeleteObjectsResult response) {
-		 List<KeyVersion> keyList = new ArrayList<KeyVersion>();
+		List<KeyVersion> keyList = new ArrayList<KeyVersion>();
+		
 		for (DeletedObject deletedObject : response.getDeletedObjects()) {
-            // Note that the specified version ID is the version ID for the delete marker.
-            keyList.add(new KeyVersion(deletedObject.getKey(), deletedObject.getDeleteMarkerVersionId()));
-         // Create a request to delete the delete markers.
-            DeleteObjectsRequest deleteRequest = new DeleteObjectsRequest(bucketName).withKeys(keyList).withQuiet(false);
-         // Delete the delete markers, leaving the objects intact in the bucket.
-            DeleteObjectsResult delObjRes = s3.deleteObjects(deleteRequest);
-            successfulDeletes = delObjRes.getDeletedObjects().size();
-//            if(successfulDeletes==8) {
-        }
-		 if(successfulDeletes==2) {
+            		keyList.add(new KeyVersion(deletedObject.getKey(), deletedObject.getDeleteMarkerVersionId()));
+            		DeleteObjectsRequest deleteRequest = new DeleteObjectsRequest(bucketName).withKeys(keyList).withQuiet(false);
+            		DeleteObjectsResult delObjRes = s3.deleteObjects(deleteRequest);
+            		successfulDeletes = delObjRes.getDeletedObjects().size();
+        	}
+		if(successfulDeletes==2) {
   			JOptionPane.showMessageDialog(new JDialog(), "successfulDeletes Markers!!");
-         }else {
+         	}else {
   			JOptionPane.showMessageDialog(new JDialog(), "faildDelete Markers!!");
-         }
-//         assertEquals(successfulDeletes,8);
+        	}
+	
 	}
 	public static void dialog(boolean b) {
 		if(b) {
@@ -121,7 +110,7 @@ public class DeleteObjectsforTest {
 		}else {
 			JOptionPane.showMessageDialog(new JDialog(), "faild!!");
 		}
+	
 	}
-
 
 }
