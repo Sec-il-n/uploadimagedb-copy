82f51f9d3f583084ae7e6faf842a0e0ad8e6b411
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 07:47:24 +0900

    Update ToLoginResult.java

--- src/main/java/servlet/ToLoginResult.java
+++ src/main/java/servlet/ToLoginResult.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,16 +8,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import model.beans.Account;
 
 @WebServlet("/ToLoginResult")
 public class ToLoginResult extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public ToLoginResult() {
-        super();
-    }
+	public ToLoginResult() {
+		super();
+	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String to=null;
@@ -27,6 +25,7 @@ public class ToLoginResult extends HttpServlet {
 		HttpSession session=request.getSession();
 		Account account=(Account) session.getAttribute("account");
 		msg=(String) session.getAttribute("errormsg");
+		
 		if(msg==null){
 			msg=(String) session.getAttribute("msg");
 		}
@@ -42,32 +41,30 @@ public class ToLoginResult extends HttpServlet {
 
 		request.setAttribute("msg",msg);
 		session.removeAttribute("msg");
-
-		//if()でfrom  UploadComp、rewrite //(消すもの分ける)?
 		session.removeAttribute("title");
 		session.removeAttribute("text");
 		session.removeAttribute("filename");
-		session.removeAttribute("base64");//
-		//
-		//showImageから
-		session.removeAttribute("mutterList");//
+		session.removeAttribute("base64");
+		session.removeAttribute("mutterList");
 		//EditImageから
-		session.removeAttribute("deleteFileName");//
-		session.removeAttribute("deleteFilePath");//
-		session.removeAttribute("time");//
+		session.removeAttribute("deleteFileName");
+		session.removeAttribute("deleteFilePath");
+		session.removeAttribute("time");
 		//ShowImagesから
-		session.removeAttribute("total");//
-		session.removeAttribute("totalPage");//
+		session.removeAttribute("total");
+		session.removeAttribute("totalPage");
 		//Deleteから
-		session.removeAttribute("newFileName");//
+		session.removeAttribute("newFileName");
 		session.removeAttribute("errormsg");
 
 		RequestDispatcher dsp=request.getRequestDispatcher(to);
-	    dsp.forward(request, response);
+	    	dsp.forward(request, response);
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		doGet(request, response);
 	}
 
