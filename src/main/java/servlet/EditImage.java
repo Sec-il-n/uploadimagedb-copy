bac49a39011880de869340e9da88927c25c6a853
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 06:37:03 +0900

    Update EditImage.java

--- src/main/java/servlet/EditImage.java
+++ src/main/java/servlet/EditImage.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.IOException;
 import java.nio.file.Path;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -11,21 +10,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import model.logic.GetPathLogic;
 import model.logic.GetTimeFileCreatedLogic;
 
 @WebServlet("/EditImage")
 @MultipartConfig(maxFileSize=10485760,location="/tmp/")
+
 public class EditImage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public EditImage() {
-        super();
-    }
+	public EditImage() {
+		super();
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String i = request.getParameter("id");//null
+		
+		String i = request.getParameter("id");
 		int id = Integer.parseInt(i);
 		String title = request.getParameter("title");
 		String text = request.getParameter("text");
@@ -33,46 +33,25 @@ public class EditImage extends HttpServlet {
 
 		GetTimeFileCreatedLogic gtlogic=new GetTimeFileCreatedLogic ();
 		String time=gtlogic.getTime(deleteFileName);
-//get path to delete
 		GetPathLogic gplogic=new GetPathLogic();
 		Path path=gplogic.getDir();
 		Path deleteFilePath=gplogic.getFilePath(path, deleteFileName);
-//		String deleteFilePath=path.toString() + "/"+ deleteFileName;
 
-//		ImageBean imgb=new ImageBean(id,title,text);
-//		request.setAttribute("imgb",imgb);
-
-		HttpSession session = request.getSession();//いご処理で必要
+		HttpSession session = request.getSession();
 		session.setAttribute("deleteFileName", deleteFileName);
 		session.setAttribute("time", time);
 		session.setAttribute("deleteFilePath", deleteFilePath);
-
 		request.setAttribute("idrewrite", id);
 		request.setAttribute("title", title);
 		request.setAttribute("text", text);
 		RequestDispatcher dsp=request.getRequestDispatcher("/WEB-INF/jsp/rewriteImage.jsp");
 		dsp.forward(request, response);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		String deleteFileName=request.getParameter("deleteFileName");
-//
-//		GetPathLogic gplogic=new GetPathLogic();
-//		Path path=gplogic.getPath();
-//
-//		GetTimeFileCreatedLogic gtlogic=new GetTimeFileCreatedLogic ();
-//		String time=gtlogic.getTime(deleteFileName);
-//		String deleteFilePath=path.toString() + "/"+ deleteFileName;
-//
-//		HttpSession session = request.getSession();
-//		session.setAttribute("deleteFileName", deleteFileName);
-//		session.setAttribute("time", time);
-//		session.setAttribute("deleteFilePath", deleteFilePath);
-//
-//		RequestDispatcher dsp=request.getRequestDispatcher("/WEB-INF/jsp/rewriteImage.jsp");
-//		dsp.forward(request, response);
 
 		doGet(request, response);
 	}
