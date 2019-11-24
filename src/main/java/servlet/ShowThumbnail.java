046180f5a216094b0623cc835552da0fce65d208
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 07:41:08 +0900

    Update ShowThumbnail.java

--- src/main/java/servlet/ShowThumbnail.java
+++ src/main/java/servlet/ShowThumbnail.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.IOException;
 import java.nio.file.Path;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,35 +9,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import model.logic.CheckDirectoryContentsLogic;
 import model.logic.LoginLogic;
 
 @WebServlet("/ShowThumbnail")
 public class ShowThumbnail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    public ShowThumbnail() {
-        super();
-    }
+	public ShowThumbnail() {
+		super();
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String to = null;
 		HttpSession session = request.getSession();
+		String to = null;
 		Path tmpPath=(Path) session.getAttribute("tmpPath");
-		//add 9/19
 		String userId=(String) session.getAttribute("userId");
+		
 		LoginLogic llogic=new LoginLogic();
 		to = llogic.sessionCheck(userId);
 		String msg = llogic.getMsg(to);
-//		if(to!=null && msg!=null){
 
-//			request.setAttribute("msg", msg);
-//			RequestDispatcher dsp=request.getRequestDispatcher(to);
-//			dsp.forward(request, response);
-//		}else {
 		if(to==null && msg==null){
 			CheckDirectoryContentsLogic logic=new CheckDirectoryContentsLogic();
 			boolean exist=logic.checkDirectry(tmpPath);
+			
 			if(!exist){
 				to="/WEB-INF/jsp/fileUpload.jsp";
 				msg="入力し直して下さい。";//
@@ -47,6 +41,7 @@ public class ShowThumbnail extends HttpServlet {
 				to="/WEB-INF/jsp/thumbnail.jsp";
 			}
 		}
+		
 		request.setAttribute("msg", msg);
 		RequestDispatcher dsp=request.getRequestDispatcher(to);
 		dsp.forward(request, response);
