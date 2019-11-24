0fc641c10bb11f50939ef6f45911d42510988ecd
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 05:14:55 +0900

    Update RegisterDAO.java

--- src/main/java/dao/RegisterDAO.java
+++ src/main/java/dao/RegisterDAO.java
@@ -14,44 +14,31 @@ import model.beans.Account;
 public class RegisterDAO{
 	static final String URL = System.getenv("JDBC_URL");
 	static final String USERNAME = System.getenv("DB_USERNAME");
-    static final String PASSWORD = System.getenv("DB_PASSWORD");
+    	static final String PASSWORD = System.getenv("DB_PASSWORD");
 
-//    private final MysqlDataSource source;
-    private final DataSource source;
+    	private final DataSource source;
 
-
-    public RegisterDAO(MysqlDataSource source){
-    	this.source=source;
-    }
-	public RegisterDAO(DataSource source){
-	    this.source=source;
+	public RegisterDAO(MysqlDataSource source){
+		this.source=source;
 	}
-
-//	public void properties() throws IOException {
-//		GetDataSourceLogic logic=new GetDataSourceLogic();
-//		Properties props=logic.getProperties();
-//		URL=props.getProperty("JDBC_URL");
-//		USERNAME=props.getProperty("DB_USERNAME");
-//		PASSWORD=props.getProperty("DB_PASSWORD");
-//
-//	}
+	public RegisterDAO(DataSource source){
+	    	this.source=source;
+	}
 
 	public Boolean register(Account ac){
 
 		Connection conn=null;
-
 		String userId=ac.getUserId();
-	    String pass=ac.getPass();
-	    String name=ac.getName();
-	    int age=ac.getAge();
+		String pass=ac.getPass();
+		String name=ac.getName();
+		int age=ac.getAge();
 
-	    try {
-//			conn=source.getConnection();
+		try {
 			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
 
 			String sql="INSERT INTO ACCOUNT (userId,pass,name,age) VALUES(?,?,?,?);";
 			PreparedStatement st=conn.prepareStatement(sql);
+			
 			st.setString(1,userId);
 			st.setString(2,pass);
 			st.setString(3,name);
@@ -60,24 +47,24 @@ public class RegisterDAO{
 
 			if(result==1){
 				return true;
-//				return account;
 			}
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return false;
-//			return null;
+
 		}finally{
-		    try {
+		    
+			try {
 				if(conn!=null){
 				    conn.close();
 				}
+			
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
 	    return false;
-//		return account;
 	}
 
 
@@ -86,18 +73,17 @@ public class RegisterDAO{
 		Connection conn=null;
 		String userId=old.getUserId();
 		String pass=old.getPass();
-
 		String newUserId=ac.getUserId();
 		String newPass=ac.getPass();
 		String newName=ac.getName();
-	    int newAge=ac.getAge();
+		int newAge=ac.getAge();
 
 		try {
-//			conn=source.getConnection();
 			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
 			String sql="UPDATE ACCOUNT SET userId=?, pass=?, name=?, age=? WHERE userId=? AND pass=?";
 			PreparedStatement st=conn.prepareStatement(sql);
+			
 			st.setString(1,newUserId);
 			st.setString(2,newPass);
 			st.setString(3,newName);
@@ -128,16 +114,15 @@ public class RegisterDAO{
 	public boolean registerDelete(Account ac){
 
 		Connection conn=null;
-
 		String userId=ac.getUserId();
 		String pass=ac.getPass();
 
 		try {
-//			conn=source.getConnection();
 			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
 			String sql="DELETE FROM ACCOUNT WHERE userId=? AND pass=?";
 			PreparedStatement st=conn.prepareStatement(sql);
+			
 			st.setString(1,userId);
 			st.setString(2,pass);
 			int result=st.executeUpdate();
