959c7a945fc4b4c6b27edde5a4bac20f1528c209
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 05:24:14 +0900

    Update LoginDAO.java

--- src/main/java/dao/LoginDAO.java
+++ src/main/java/dao/LoginDAO.java
@@ -5,28 +5,28 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import javax.sql.DataSource;
-
 import model.beans.Account;
 
 public class LoginDAO{
+	
 	static final String URL = System.getenv("JDBC_URL");
-    static final String USERNAME = System.getenv("DB_USERNAME");
-    static final String PASSWORD = System.getenv("DB_PASSWORD");
-
+    	static final String USERNAME = System.getenv("DB_USERNAME");
+   	static final String PASSWORD = System.getenv("DB_PASSWORD");
 	private final DataSource source;
 
 	public LoginDAO(DataSource source){
 		this.source=source;
 	}
+	
 	public Account findId(Account ac) throws ClassNotFoundException {
+	
 		Connection conn=null;
-	    String userId;
-	    String pass;
-	    String name;
-	    int age;
-	    Account account=null;
+		String userId;
+		String pass;
+		String name;
+		int age;
+		Account account=null;
 
 	    try {
 
@@ -37,12 +37,13 @@ public class LoginDAO{
 			st.setString(1,ac.getUserId());
 			st.setString(2,ac.getPass());
 			ResultSet rs=st.executeQuery();
-			if(rs.next()){
-					userId=rs.getString("USERID");
-					pass=rs.getString("PASS");
-					name=rs.getString("NAME");
-					age=rs.getInt("AGE");
-					account=new Account(userId,pass,name,age);
+			
+		    	if(rs.next()){
+				userId=rs.getString("USERID");
+				pass=rs.getString("PASS");
+				name=rs.getString("NAME");
+				age=rs.getInt("AGE");
+				account=new Account(userId,pass,name,age);
 			}
 			st.close();
 			rs.close();
@@ -50,13 +51,15 @@ public class LoginDAO{
 		} catch (NullPointerException e) {
 			e.printStackTrace();
 			throw new NullPointerException("account(引数・戻り)いずれかがNULL");
-		} catch (SQLException e) {
+		
+	   	} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}finally{
+		    
 		    try {
 				if(conn!=null){
-				    conn.close();
+				    	conn.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
@@ -66,7 +69,7 @@ public class LoginDAO{
 		return account;
 	}
 
-	}
+}
 
 
 
