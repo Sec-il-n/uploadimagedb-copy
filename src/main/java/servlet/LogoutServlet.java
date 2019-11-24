77168be97758b1439869011e87d7a497109971af
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 06:42:06 +0900

    Update LogoutServlet.java

--- src/main/java/servlet/LogoutServlet.java
+++ src/main/java/servlet/LogoutServlet.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,13 +11,15 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
-    public LogoutServlet() {
-        super();
-    }
+	public LogoutServlet() {
+		super();
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		HttpSession session=request.getSession();
 		session.invalidate();
 
