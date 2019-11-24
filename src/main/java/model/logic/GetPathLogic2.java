86de2207f11c5f8d6293597199b89d351eff9856
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 06:25:34 +0900

    Update GetPathLogic2.java

--- src/main/java/model/logic/GetPathLogic2.java
+++ src/main/java/model/logic/GetPathLogic2.java
@@ -6,51 +6,26 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 public class GetPathLogic2 {
-	/**
-	 * 0.create Dir /image/upload
-	 * 1. get 'File' from tmp
-	 * 2. write
-	 * dist:  pass to upload(out side of the 'webapps')
-	 */
-	//need createDir()
-
-	/**
-	 * ⤴2
-	 * move to create file logic
-	 */
-//	public void execute(Path tmpPath,Path upPath) {
-//		Files.copy(tmpPath, upPath, REPLACE_EXISTING);
-//
-//	}
-//
-//	}
-
-
+	
 	/**
 	 * パスを、ブラウザから開くことのできる文字列に変換する必要がある場合は、toUriを使用
 	 */
 	public String showURL() {
-		Path p=Paths.get("/");// Pathに対応するファイルが存在している必要はありません。
+		Path p=Paths.get("/");
 	 	String path = p.toUri().getPath();
 	 	return path;
 	}
 
-/**
- * show Image it woudn't be acsessed
- * download =>reloadしないと2回連続でDLできない。(一度stream使うと x)
- */
 	HttpServletResponse resp;
 	public void acsessFile(Path tmpPath,HttpServletResponse resp) throws IOException{
+		
 		File file = new File (tmpPath.toString());
 		resp.setHeader("Content-Type", "image/*" );
-		//Content-Disposition ?? is downloaded and saved locally.
-//		resp.setHeader("Content-Disposition","inline" );//メッセージ本文の一部として表示されるコンテンツ??->DLされた
-		resp.setHeader("Content-Disposition","attachment; filename=\"sample.jpg\"" );//<=>別ファイルとして添付
+		resp.setHeader("Content-Disposition","attachment; filename=\"sample.jpg\"" );
 		resp.setContentLength((int)file.length());
 
 		try(BufferedInputStream in=new BufferedInputStream(new FileInputStream(file));
@@ -62,8 +37,6 @@ public class GetPathLogic2 {
 			}
 		}
 
-//		String to="";
-//		resp.sendRedirect(to);
 
 	}
 
