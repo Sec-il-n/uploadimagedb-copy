a1e1d84c5f1e42c6345429a66ab13e8977445c6b
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 06:49:24 +0900

    Update RegisterDelete.java

--- src/main/java/servlet/RegisterDelete.java
+++ src/main/java/servlet/RegisterDelete.java
@@ -10,26 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
-
 import model.beans.Account;
 import model.logic.GetDataSourceLogic;
 import model.logic.LoginLogic;
 import model.logic.RegisterLogic;
 
-/**
- * Servlet implementation class RegisterDelete
- */
 public class RegisterDelete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterDelete() {
-        super();
-    }
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+	public RegisterDelete() {
+        	super();
+    	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		HttpSession session = request.getSession();
@@ -39,16 +31,17 @@ public class RegisterDelete extends HttpServlet {
 		LoginLogic llogic=new LoginLogic();
 		String to = llogic.sessionCheck(userId);
 		String msg = llogic.getMsg(to);
+		
 		if(msg!=null){
+			
 			request.setAttribute("msg", msg);
 		}
 
 		if(userId!=null && account!=null) {
 
-//			RegisterLogic logic=new RegisterLogic(dataSource);
-
-		    GetDataSourceLogic gdlogic=new GetDataSourceLogic();
-		    DataSource dataSource = null;
+			GetDataSourceLogic gdlogic=new GetDataSourceLogic();
+			DataSource dataSource = null;
+			
 			try {
 				dataSource = gdlogic.getDataSource();
 			} catch (NamingException e) {
@@ -57,9 +50,8 @@ public class RegisterDelete extends HttpServlet {
 
 			boolean resultDelete = false;
 			RegisterLogic logic=new RegisterLogic(dataSource);
+			
 			try {
-//				resultDelete = logic.delete(account,dataSource);
-
 				resultDelete = logic.delete(account);
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -67,31 +59,19 @@ public class RegisterDelete extends HttpServlet {
 
 			msg=logic.deleteMsg(resultDelete);
 			to=logic.deleteRgisterPath(resultDelete);
-//			if(resultDelete) {
-////				session.invalidate();
-//				session.removeAttribute("userId");
-//				session.removeAttribute("account");
-//				msg="登録を取り消しました。";
-//			    to="/";
-//
-//			}else {
-//				msg="書き換えに失敗しました。やり直して下さい。";//'d set primary @db
-//		    	to="/ToLoginResult";
-//			}
 
 		}else {
 			RequestDispatcher dsp=request.getRequestDispatcher(to);
-		    dsp.forward(request, response);
+		    	dsp.forward(request, response);
 		}
+		
 		session.setAttribute("deletemsg", msg);
 		response.setHeader("Cache-Control", "no-cache");
 		response.sendRedirect(to);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
