bdd6a107512179befa217d31db49b097a7d830c3
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 07:26:50 +0900

    Update ShowImages.java

--- src/main/java/servlet/ShowImages.java
+++ src/main/java/servlet/ShowImages.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
-
 import model.beans.ImageBean;
 import model.logic.GetAllImageFromDBLogic;
 import model.logic.GetDataSourceLogic;
@@ -23,10 +21,9 @@ import model.logic.PageNationLogic;
 @WebServlet("/ShowImages")
 public class ShowImages extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    public ShowImages() {
-        super();
-    }
-//parameter=無し,"show","edit"の三分岐
+    	public ShowImages() {
+        	super();
+    	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String action=request.getParameter("action");
@@ -38,18 +35,21 @@ public class ShowImages extends HttpServlet {
 		LoginLogic logic=new LoginLogic();
 		path = logic.sessionCheck(userId);
 		msg = logic.getMsg(path);
+		
 		if(msg!=null){
 			request.setAttribute("msg", msg);
 		}
 
 		if(userId!=null) {
-		    GetDataSourceLogic gdlogic=new GetDataSourceLogic();
-		    DataSource dataSource = null;
+		    	GetDataSourceLogic gdlogic=new GetDataSourceLogic();
+		    	DataSource dataSource = null;
+			
 			try {
 				dataSource = gdlogic.getDataSource();
 			} catch (NamingException e) {
 				e.printStackTrace();
 			}
+			
 			GetAllImageFromDBLogic gilogic = new GetAllImageFromDBLogic(dataSource);
 			GetPageLogic gplogic = new GetPageLogic(dataSource);
 			PageNationLogic pnlogic =new PageNationLogic();
@@ -61,41 +61,31 @@ public class ShowImages extends HttpServlet {
 			try {
 				total = filesList.size();
 			} catch (Exception e) {
-//				path="/WEB-INF/jsp/errorpage.jsp";
 				path=gplogic.toError();
 				RequestDispatcher dsp=request.getRequestDispatcher(path);
 				dsp.forward(request, response);
 				e.printStackTrace();
 			}
 			session.setAttribute("total", total);
-//			GetPageLogic gplogic = new GetPageLogic(dataSource);
-//			int totalPage;
-//			int in ;
+			
 			if(action.equals("show")) {
-				//1ページ目
 				int page=1;
 				List<ImageBean> pagedList=gplogic.findPage(page);
-				//pageNation 表示部分
 				totalPage = pnlogic.getTotalPageCount(total);
 				in = pnlogic.getPageShowing(page, total, totalPage);
-
-//				session.setAttribute("total", total);//
-				session.setAttribute("totalPage", totalPage);//
+				session.setAttribute("totalPage", totalPage);
 				request.setAttribute("page", page);
 				request.setAttribute("pagedList", pagedList);
-				request.setAttribute("in", in);//
-
-				//				path="/WEB-INF/jsp/showImages.jsp";
+				request.setAttribute("in", in);
 				path=gplogic.toShow();
 
-			}else if(action.equals("edit")) {///*
+			} else if(action.equals("edit")) {
 				int page=1;
-				List<ImageBean> yourPostedList = gilogic.listByUser(userId);//all
+				List<ImageBean> yourPostedList = gilogic.listByUser(userId);
 				List<ImageBean> pagedPostedList=gplogic.findPageByUser(userId, page);
 				try {
 					total = yourPostedList.size();
 				} catch (Exception e) {
-//					path="/WEB-INF/jsp/errorpage.jsp";
 					path=gplogic.toError();
 					RequestDispatcher dsp=request.getRequestDispatcher(path);
 					dsp.forward(request, response);
@@ -105,16 +95,16 @@ public class ShowImages extends HttpServlet {
 				totalPage = pnlogic.getTotalPageCountEdit(total);
 				in=pnlogic.getEditPageShowing(page, total, totalPage);
 
-				session.setAttribute("total", total);//rewrite total count of all posts
+				session.setAttribute("total", total);
 				session.setAttribute("totalPage", totalPage);
 				request.setAttribute("page", page);
-				request.setAttribute("in", in);//
+				request.setAttribute("in", in);
 				request.setAttribute("pagedPostedList", pagedPostedList);
 
-//				path="/WEB-INF/jsp/getEditFile.jsp";
 				path=gplogic.toEdit();
 			}
 		}
+		
 		RequestDispatcher dsp=request.getRequestDispatcher(path);
 		dsp.forward(request, response);
 
