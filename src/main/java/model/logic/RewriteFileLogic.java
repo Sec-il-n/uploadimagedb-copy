371d67c077f076631b49f17ac7a45c3dbffd0f32
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 06:33:54 +0900

    Update RewriteFileLogic.java

--- src/main/java/model/logic/RewriteFileLogic.java
+++ src/main/java/model/logic/RewriteFileLogic.java
@@ -8,30 +8,8 @@ import java.nio.file.StandardCopyOption;
 import java.util.Base64;
 
 public class RewriteFileLogic {
-//	public String execute(Path delete) throws IOException {
-//		boolean result;
-//		String msg=null;
-//
-//		try {
-//			result = Files.deleteIfExists(delete);
-//		} catch (DirectoryNotEmptyException e) {
-//			e.printStackTrace();
-//			throw new DirectoryNotEmptyException("ファイルがディレクトリで、ディレクトリが空でないために削除できなかった");
-//		} catch (IOException  e) {
-//		e.printStackTrace();
-//		throw new IOException("入出力エラー");
-//		}
-//		if(result) {
-//			msg="書き換えが完了しました。";
-//		}else {
-//			msg="書き換え(ファイル削除)に失敗しました。";
-//		}
-//		return msg;
-//	}
-
 	public  String rewrite(String base64 ,Path deleteFilePath)  {
-//	public void rewrite(Path path,Path deleteFilePath) throws IOException {
-		byte[] buff=Base64.getDecoder().decode(base64);//ネストクラス(Base64.Decoder=非static参照)
+		byte[] buff=Base64.getDecoder().decode(base64);
 		ByteArrayInputStream in=new ByteArrayInputStream(buff);
 		String msg;
 		try {
@@ -44,4 +22,5 @@ public class RewriteFileLogic {
 		return msg;
 
 	}
+
 }
