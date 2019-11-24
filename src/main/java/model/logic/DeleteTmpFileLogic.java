a67a29ac80dbb255f12b691948a8f90a77459c44
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 05:57:07 +0900

    Update DeleteTmpFileLogic.java

--- src/main/java/model/logic/DeleteTmpFileLogic.java
+++ src/main/java/model/logic/DeleteTmpFileLogic.java
@@ -13,32 +13,15 @@ public class DeleteTmpFileLogic extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public void DeleteTmpFile(Path tmpPath) {
-			//if delete each file
-		    try {
-		    	File file=new File(tmpPath.toString());
-				file.delete();//やり直した分は消えない
-//				Files.delete(tmpPath);//やり直した分は消えない
+	public void DeleteTmpFile(Path tmpPath) {
+		try {
+			File file=new File(tmpPath.toString());
+			file.delete();
 
-		    } catch (Exception e) {
-		    	e.printStackTrace();
-//		    } catch (NoSuchFileException e) {
-//				e.printStackTrace();
-//
-//		    } catch (IOException e) {
-//				e.printStackTrace();
-			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
     }
-//    public void DeleteTmpFile(List<String> filesTmpPath) {
-//    	for(String f:filesTmpPath) {
-//    		try {
-//    			Files.delete(Paths.get(f));
-//    		} catch (IOException e) {
-//    			e.printStackTrace();
-//    		}
 
-
-//    	}
-//    }
 }
