4200baa3f8af66c8311eca764b79838bd41edffb
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 07:44:09 +0900

    Update ToFileUploadJsp.java

--- src/main/java/servlet/ToFileUploadJsp.java
+++ src/main/java/servlet/ToFileUploadJsp.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,17 +11,19 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/ToFileUploadJsp")
 public class ToFileUploadJsp extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
-    public ToFileUploadJsp() {
-        super();
-    }
+	public ToFileUploadJsp() {
+		super();
+	}
 
     @Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	String path;
-    	HttpSession session = request.getSession();
+    		HttpSession session = request.getSession();
+		String path;
 		String userId=(String) session.getAttribute("userId");
+		
 		if(userId==null) {
 			String msg="ログインし直して下さい。";
 			request.setAttribute("msg", msg);
@@ -35,6 +36,7 @@ public class ToFileUploadJsp extends HttpServlet {
 		RequestDispatcher dsp=request.getRequestDispatcher(path);
 		dsp.forward(request, response);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
