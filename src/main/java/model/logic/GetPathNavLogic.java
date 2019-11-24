6150e6375fb2a6f0ae7fa4be49d8146c4c951356
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 06:26:30 +0900

    Update GetPathNavLogic.java

--- src/main/java/model/logic/GetPathNavLogic.java
+++ src/main/java/model/logic/GetPathNavLogic.java
@@ -3,14 +3,17 @@ package model.logic;
 import javax.servlet.http.HttpServlet;
 
 public class GetPathNavLogic extends HttpServlet {
+	
 	public String getNavPath(String action) {
 		String path = null;
+		
 		if(action.equals("start")) {
 			path="/WEB-INF/jsp/howtostart.jsp";
 
 		}else if(action.equals("comment")) {
 			path="/WEB-INF/jsp/howtocoment.jsp";
 		}
+		
 		return path;
 
 	}
