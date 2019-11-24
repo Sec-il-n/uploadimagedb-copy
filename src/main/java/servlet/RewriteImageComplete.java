e6ec2831428683a59ba12a549712ad980054a211
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 07:12:44 +0900

    Update RewriteImageComplete.java

--- src/main/java/servlet/RewriteImageComplete.java
+++ src/main/java/servlet/RewriteImageComplete.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.IOException;
 import java.nio.file.Path;
-
 import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
-
 import model.beans.ImageBean;
 import model.beans.Mutter;
 import model.logic.CreateFileLogic;
@@ -23,23 +21,24 @@ import model.logic.GetDataSourceLogic;
 import model.logic.LoginLogic;
 import model.logic.RewriteFileLogic;
 import model.logic.UpdateFileDBLogic;
-//現s3と併用しているのでdeleteFilePath(folder uploadの処理不要(?))
+
 @WebServlet("/RewriteImageComplete")
 public class RewriteImageComplete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public RewriteImageComplete() {
-        super();
-    }
+	public RewriteImageComplete() {
+		super();
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		HttpSession session = request.getSession();
 		Path deleteFilePath = (Path) session.getAttribute("deleteFilePath");
-		Path tmpPath = (Path) session.getAttribute("tmpPath");//?
+		Path tmpPath = (Path) session.getAttribute("tmpPath");
+		
 		String title = (String) session.getAttribute("title");
 		String text = (String) session.getAttribute("text");
-		String filename=(String) session.getAttribute("filename");//from user
+		String filename=(String) session.getAttribute("filename");
 		String deleteFileName=(String) session.getAttribute("deleteFileName");
 		String postedtime=(String) session.getAttribute("time");
 		String base64=(String) session.getAttribute("base64");//new
@@ -55,61 +54,49 @@ public class RewriteImageComplete extends HttpServlet {
 		}
 
 		if(userId!=null){
-//⤴ブラウザで戻ると︎getAttributeしないので↓かかる(need to code ブラウザ戻る)
 			if(deleteFilePath==null|| title==null||text==null||deleteFileName==null||postedtime==null ||base64==null) {
-	//		if(deleteFilePath==null|| title==null||text==null||tmpPath==null||filename==null||deleteFileName==null||postedtime==null) {
 				session.setAttribute("errormsg", "不正な画面移管が有りました。(更新が完了しているか、内容が取得できていない可能性があります。)");
 				response.setHeader("Cache-Control", "no-cache");
 				response.sendRedirect("/ToLoginResult");
-	//
-	//		}else if(deleteFilePath!=null&&title!=null&&text!=null&&tmpPath!=null&&filename!=null&&deleteFileName!=null&&postedtime!=null) {
-			}else if(deleteFilePath!=null && title!=null && text!=null && filename!=null && deleteFileName!=null && postedtime!=null) {
-				//**
-	//			GetPathLogic gplogic=new GetPathLogic();
-	//			//1
-	//			Path p=gplogic.getDir();
+			} else if(deleteFilePath!=null && title!=null && text!=null && filename!=null && deleteFileName!=null && postedtime!=null) {
 				CreateFileLogic clogic=new CreateFileLogic();
 				String newFileName = clogic.getRewritedFileName(postedtime, filename);//
-	//			Path createdFile=clogic.createFile(p, newFileName);//
-	//			//2
-	//			Path path=gplogic.getPath(deleteFilePath);//**
 				RewriteFileLogic dlogic=new RewriteFileLogic();
 				msg=dlogic.rewrite(base64,deleteFilePath);
-	//			String msg=dlogic.execute(path);//
 
-				//DataSource
 				GetDataSourceLogic gdlogic=new GetDataSourceLogic();
-			    DataSource dataSource = null;
+			    	DataSource dataSource = null;
+				
 				try {
 					dataSource = gdlogic.getDataSource();
 				} catch (NamingException e) {
 					e.printStackTrace();
 				}
-				//db
+				
 				FindIdFromDBLogic filogic = new FindIdFromDBLogic();
 				int id = filogic.execute(deleteFileName,dataSource);
-
-	//			FileUploadLogic flogic=new FileUploadLogic();//
-	//			flogic.execute(tmpPath,createdFile);//
 
 				ImageBean bean1 = new ImageBean(id,title,newFileName);
 				Mutter bean2 = new Mutter(id,title,text);
 				UpdateFileDBLogic uflogic=new UpdateFileDBLogic(dataSource);
+				StringBuilder sb=new StringBuilder(msg);
+				
 				String msg1 = uflogic.rewriteImage(bean1);
 				String msg2 = uflogic.rewriteMutter(bean2);
-				StringBuilder sb=new StringBuilder(msg);
 				sb.append(msg1).append(System.lineSeparator()).append(msg2);
-				//s3
+				
 				FileUploadS3Logic s3uplogic=new FileUploadS3Logic();
 				DeleteFileS3Logic s3logic=new DeleteFileS3Logic ();
 				s3uplogic.s3Upload(newFileName, tmpPath.toString());
 				s3logic.s3Delete(deleteFileName);
 
 				session.setAttribute("msg",sb.toString());
-				session.setAttribute("newFileName",newFileName);//
+				session.setAttribute("newFileName",newFileName);
+				
 				response.setHeader("Cache-Control", "no-cache");
 				response.sendRedirect("/ToLoginResult");
 			}
+		
 		}
 
 		response.getWriter().append("Served at: ").append(request.getContextPath());
