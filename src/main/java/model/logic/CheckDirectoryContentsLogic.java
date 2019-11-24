cfc55790ab7fa5d9f5f62e46db0fbc618316c55c
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 05:48:01 +0900

    Update CheckDirectoryContentsLogic.java

--- src/main/java/model/logic/CheckDirectoryContentsLogic.java
+++ src/main/java/model/logic/CheckDirectoryContentsLogic.java
@@ -12,62 +12,62 @@ import java.util.TreeMap;
 
 public class CheckDirectoryContentsLogic {
 
-//filenameをtaimestamp降順に並べる//確認用?不要?
 	public List<String> sortedFileName(List<String> fileName) throws IOException{
+		
 		Map<String,String> filesMap=new TreeMap<>(Comparator.reverseOrder());
+		
 		for(String f:fileName) {
 			filesMap.put(f.substring(0,12),f);
 		}
+		
 		List<String> sortedFileName=new ArrayList<>();
+		
 		for(String s:filesMap.values()) {
 			sortedFileName.add(s);
 		}
+		
 		return sortedFileName;
 	}
+	
 	public boolean checkDirectry(Path path) {//ファイルまでのパス
+	
 		boolean exists = false;
+		
 		try {
 			exists=Files.exists(path);
+		
 		} catch (NullPointerException e) {
 			e.printStackTrace();
 			return false;
 		}
+		
 		if(exists) {
 			return true;
+		
 		}else {
 			return false;
 		}
 	}
-//以下不要
-//showimages1
+
 	public List<String> filespath(Path path) throws IOException{
+	
 		File[] files=new File(path.toString()).listFiles();
 		List<String> filesPath=new ArrayList<>();
+		
 		if(files!=null){
 			for(File f:files) {
-				filesPath.add(f.getAbsolutePath());//get()で渡せば抽象にはならない
+				filesPath.add(f.getAbsolutePath());
 			}
 		}
 		return filesPath;
 	}
 
-//
-//	Set<String> =getResourcePaths(String path);**
 
 	public File[] filespath(String path) throws IOException{
 		File[] files=new File(path).listFiles();
 		return files;
     }
-//戻りbooleanにして、SERVLETで分岐なら使用可
-//	public String checkDirectry(Path path) {//ファイルまでのパス
-//		String msg=path.toAbsolutePath().toString();
-//		if(Files.exists(path)) {
-//			msg=msg+"は存在します";
-//		}else {
-//			msg=msg+"は存在しません";
-//		}
-//		return msg;
-//	}
+
 }
 
 
