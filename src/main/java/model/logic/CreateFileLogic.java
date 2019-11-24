0ba12927fa1df1b5ceb66ff44df1275fc61d6235
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 05:53:22 +0900

    Update CreateFileLogic.java

--- src/main/java/model/logic/CreateFileLogic.java
+++ src/main/java/model/logic/CreateFileLogic.java
@@ -9,90 +9,89 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CreateFileLogic {
+	
 	public Path createDirectry(Path path) throws IOException {
+
 		Path createdDirectry=null;
+		
 		try {
 			createdDirectry=Files.createDirectories(path);//すでに存在していて作成できなかった場合でも例外はスローされません。<->createDirectory
 
 		} catch (UnsupportedOperationException e1) {
 			e1.printStackTrace();
 			throw new UnsupportedOperationException("ディレクトリの作成時に原子的に設定できない属性が配列に含まれる");
+		
 		} catch (FileAlreadyExistsException  e1) {
 			e1.printStackTrace();
 			throw new FileAlreadyExistsException ("dirが存在するが、ディレクトリではない(オプションの固有例外)");
+		
 		} catch (IOException  e1) {
 			e1.printStackTrace();
 			throw new IOException ("入出力エラー");
 		}
+		
 		return createdDirectry;
 	}
+	
 	/**
 	 * tmpFile
 	 */
-//		public Path createFile(Path dir) throws IOException {
-			public Path createFile(Path dir,String suffix) throws IOException {
+	public Path createFile(Path dir,String suffix) throws IOException {
 
-			Path createdfile = null;
-			try {
-				createdfile=Files.createTempFile(dir, null, suffix);
+		Path createdfile = null;
 
-			} catch (IllegalArgumentException  e) {
-				e.printStackTrace();
-				throw new IllegalArgumentException   ("新しいファイル名にsuffixを使用できません");
-			} catch (UnsupportedOperationException  e) {
-				e.printStackTrace();
-				throw new UnsupportedOperationException  ("原子的に設定できない属性が配列に含まれます");
-			}catch (IOException e) {
-				e.printStackTrace();
-			}
-			return createdfile;
+		try {
+			createdfile=Files.createTempFile(dir, null, suffix);
+
+		} catch (IllegalArgumentException  e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException   ("新しいファイル名にsuffixを使用できません");
+
+		} catch (UnsupportedOperationException  e) {
+			e.printStackTrace();
+			throw new UnsupportedOperationException  ("原子的に設定できない属性が配列に含まれます");
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		public String getCreatedFileName(String filename) {
-			long time=System.currentTimeMillis();
-			String nameadd  = new StringBuilder(String.valueOf(time)).append("_").toString();
-			return nameadd + filename;
-		}
-//現未使用
-//	public Path createTmpDirectry(Path path) throws IOException {
-//		Path createdDirectry=null;
-//		try {
-//			createdDirectry=Files.createTempDirectory(path, null);//すでに存在していて作成できなかった場合でも例外はスローされません。<->createDirectory
-//
-//		} catch (UnsupportedOperationException e1) {
-//			e1.printStackTrace();
-//			throw new UnsupportedOperationException("ディレクトリの作成時に原子的に設定できない属性が配列に含まれる");
-//		} catch (IOException  e1) {
-//			e1.printStackTrace();
-//			throw new IOException ("入出力エラー");
-//		} catch (Exception e1) {
-//			e1.printStackTrace();
-//		}
-//		return createdDirectry;
-//	}
-//✔︎Path.resolve()　検証
-//	dir=glogic.getTmpPath(ServletContext context);
+
+		return createdfile;
+	}
+		
+	public String getCreatedFileName(String filename) {
+
+		long time=System.currentTimeMillis();
+		String nameadd  = new StringBuilder(String.valueOf(time)).append("_").toString();
+		return nameadd + filename;
+	}
+
 	public Path createUploadFile(Path dir,String filename) throws IOException {
-//		Path createdfile = Paths.get(path.toString(),filename);//null
+		
 		Path createdfile = dir.resolve(filename);//null
 
 		try {
 			createdfile=Files.createFile(createdfile);
+		
 		} catch (UnsupportedOperationException  e) {
 			e.printStackTrace();
 			throw new UnsupportedOperationException  ("原子的に設定できない属性が配列に含まれます");
+		
 		} catch (FileAlreadyExistsException e) {
 			e.printStackTrace();
 			throw new FileAlreadyExistsException ("同名ファイルがすでに存在します");
-		}catch (NoSuchFileException e) {
+		
+		} catch (NoSuchFileException e) {
 			e.printStackTrace();
 			throw new NoSuchFileException("ファイルが作成できませんでした");
 		}
+		
 		return createdfile;
 	}
 
 
 
 	public String getRewritedFileName(String postedtime,String filename) {
+		
 		long time=System.currentTimeMillis();
 		String nameadd  = new StringBuilder(postedtime).append("_").append(String.valueOf(time)).toString();
 		String newFileName = nameadd + filename;
@@ -102,7 +101,9 @@ public class CreateFileLogic {
 	/**
 	 * test
 	 */
+	
 	public List<String> test(Path path,Path createdFile,Path createdDir){
+		
 		List<String> test=new ArrayList<>();
 		test.add(path.toString());
 		test.add(createdFile.toString());
