52c608fefc6718d1f2a0329178c2628a798502c3
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 07:42:13 +0900

    Update ShowThumbnailEdit.java

--- src/main/java/servlet/ShowThumbnailEdit.java
+++ src/main/java/servlet/ShowThumbnailEdit.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,13 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 public class ShowThumbnailEdit extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public ShowThumbnailEdit() {
-        super();
-    }
+	public ShowThumbnailEdit() {
+		super();
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-			RequestDispatcher dsp=request.getRequestDispatcher("/WEB-INF/jsp/thumbnailEdit.jsp");
+		RequestDispatcher dsp=request.getRequestDispatcher("/WEB-INF/jsp/thumbnailEdit.jsp");
 		dsp.forward(request, response);
 
 		response.getWriter().append("Served at: ").append(request.getContextPath());
