9d5e86f7a1b1e86145adec9ab933931eca8153bb
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 06:42:59 +0900

    Update Navigate.java

--- src/main/java/servlet/Navigate.java
+++ src/main/java/servlet/Navigate.java
@@ -1,26 +1,22 @@
 package servlet;
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import model.logic.GetPathNavLogic;
-/**
- * Servlet implementation class nav
- */
+
 @WebServlet("/Navigate")
 public class Navigate extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
-    public Navigate() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	public Navigate() {
+		super();
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action=request.getParameter("action");
@@ -28,15 +24,11 @@ public class Navigate extends HttpServlet {
 		String path=logic.getNavPath(action);
 
 		RequestDispatcher dsp=request.getRequestDispatcher(path);
-	    dsp.forward(request, response);
+	    	dsp.forward(request, response);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
