bd4a2d8858a824700ae5df966011d343600f6950
Sec-il-n <52915902+Sec-il-n@users.noreply.github.com>
2019-11-25 05:17:35 +0900

    Update PagingDAO.java

--- src/main/java/dao/PagingDAO.java
+++ src/main/java/dao/PagingDAO.java
@@ -10,36 +10,37 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.sql.DataSource;
-
 import model.beans.ImageBean;
-//need to return empty list even if they don't have any comments
+
 public class PagingDAO{
+	
 	static final String URL = System.getenv("JDBC_URL");
 	static final String USERNAME = System.getenv("DB_USERNAME");
-    static final String PASSWORD = System.getenv("DB_PASSWORD");
+    	static final String PASSWORD = System.getenv("DB_PASSWORD");
 	private final DataSource source;
 
 	public PagingDAO(DataSource source){
-
 		this.source=source;
 	}
 
 	public List<ImageBean> findPage(int n){
+		
 		Connection conn=null;
 		List<ImageBean> pagedList=new ArrayList<>();
 
 		try {
 			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-//			conn=source.getConnection();
+			
 			String sql="SELECT i.ID, i.TITLE, i.FILENAME, m.USERID, m.TEXT, m.DATE_TIME FROM MUTTER_IMAGE i JOIN MUTTER m ON i.ID=m.ID "
 					+ "ORDER BY i.ID DESC LIMIT ?, 5";
 			PreparedStatement st=conn.prepareStatement(sql);
+			
 			st.setInt(1,n*5-5);
-
 			ResultSet rs=st.executeQuery();
+			
 			while(rs.next()) {
+				
 				int id=rs.getInt("id");
 				String title=rs.getString("title");
 				String filename=rs.getString("filename");
@@ -49,7 +50,8 @@ public class PagingDAO{
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
 				String date=sdf.format(tms);
 				ImageBean imgb=new ImageBean(id,title,filename,userId,text,date);
-				pagedList.add(imgb);//
+				pagedList.add(imgb);
+			
 			}
 			st.close();
 			rs.close();
@@ -57,34 +59,39 @@ public class PagingDAO{
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
+		
 		}finally{
-			try {if(conn!=null){
-				conn.close();
-			}
+			
+			try {
+				if(conn!=null){
+					conn.close();
+				}
 			}catch (SQLException e) {
 				e.printStackTrace();
 				return null;
 			}
 		}
+		
 		return pagedList;
 	}
-//✔︎getPageLogic
 	public List<ImageBean> findPageByUser(String userid,int n){
+		
 		Connection conn=null;
-
 		List<ImageBean> yourPostedList=new ArrayList<>();
 
 		try {
 			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-//			conn=source.getConnection();
+			
 			String sql="SELECT i.Id, i.title, i.filename, m.userId, m.text, m.date_time FROM mutter_image i JOIN mutter m ON i.Id=m.Id "
 					+ "WHERE m.userId=? ORDER BY i.Id DESC LIMIT ?,3";
 			PreparedStatement st=conn.prepareStatement(sql);
+			
 			st.setString(1,userid);
 			st.setInt(2,n*3-3);
 			ResultSet rs=st.executeQuery();
 
 			while(rs.next()) {
+				
 				int id=rs.getInt("id");
 				String title=rs.getString("title");
 				String filename=rs.getString("filename");
@@ -94,7 +101,8 @@ public class PagingDAO{
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
 				String date_time=sdf.format(tms);
 				ImageBean imgb=new ImageBean(id,title,filename,userId,text,date_time);
-				yourPostedList.add(imgb);//
+				yourPostedList.add(imgb);
+			
 			}
 			st.close();
 			rs.close();
@@ -102,7 +110,9 @@ public class PagingDAO{
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
+		
 		}finally{
+			
 			try {
 				if(conn!=null){
 					conn.close();
@@ -112,6 +122,7 @@ public class PagingDAO{
 				return null;
 			}
 		}
+		
 		return yourPostedList;
 	}
 }
